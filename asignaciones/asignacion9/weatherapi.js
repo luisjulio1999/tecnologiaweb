58db4a0d205e664dfaacc45569797a6d72e48834
luisjulio1999 <luis_julio_1999@hotmail.com>
2019-08-30 14:48:57 -0400

    commit finalisimo

--- asignaciones/asignacion9/weatherapi.js
+++
@@ -1,9 +0,0 @@
-let weatherRequest = new XMLHttpRequest ();
-let apiURLstring ='https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=00b6e80ee1c4c75bcf82365f49ad8f5f';
-weatherRequest.open ('GET', apiURLstring, true);
-weatherRequest.send ();
- weatherRequest.onload = function () {
- let weatherData = JSON.parse (weatherRequest.responseText);
- console.log (weatherData);
- document.getElementById ('cc-temp'). innerHTML = weatherData.main.temp;
- }
